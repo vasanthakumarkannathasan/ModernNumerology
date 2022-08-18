02fa2a7133500effe9b25e3945828970b7c283cf
VasanthaKumar Kannathasan <vasanthakumar.kannathasan@hp.com>
2022-08-18 16:40:50 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Numerology
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_ResultValue = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBox_Name = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl_Name);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.lbl_ResultValue);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtBox_Name);
+            this.groupBox1.Location = new System.Drawing.Point(46, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(820, 668);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Numerology";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Location = new System.Drawing.Point(6, 31);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(126, 20);
+            this.lbl_Name.TabIndex = 6;
+            this.lbl_Name.Text = "Enter your name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 196);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 184);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_ResultValue
+            // 
+            this.lbl_ResultValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ResultValue.Location = new System.Drawing.Point(37, 123);
+            this.lbl_ResultValue.Name = "lbl_ResultValue";
+            this.lbl_ResultValue.Size = new System.Drawing.Size(752, 38);
+            this.lbl_ResultValue.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBox_Name
+            // 
+            this.txtBox_Name.Location = new System.Drawing.Point(187, 25);
+            this.txtBox_Name.Name = "txtBox_Name";
+            this.txtBox_Name.Size = new System.Drawing.Size(420, 26);
+            this.txtBox_Name.TabIndex = 1;
+            this.txtBox_Name.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBox_Name_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 714);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Modern Numerology";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBox_Name;
+        private System.Windows.Forms.Label lbl_ResultValue;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_Name;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numerology
+{
+    public partial class Form1 : Form
+    {
+        int numerologicNumber = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numerologicNumber = 0;
+            Image file;
+            foreach (char ch in txtBox_Name.Text.ToUpperInvariant())
+            {                
+                numerologicNumber += letterValues(ch);                
+            }
+
+            if (numerologicNumber <= 108)
+            {
+                lbl_ResultValue.Text = "Your naming numerlogical number is :" + numerologicNumber;
+                file = Image.FromFile(String.Format(@"Images\{0}.PNG", +numerologicNumber));
+                pictureBox1.Image = file;
+            }
+            else
+            {
+                lbl_ResultValue.Text = "Your naming numerlogical number looks out of range :" + numerologicNumber;
+                pictureBox1.InitialImage = null;
+            }                                  
+        }
+
+        private int letterValues(char ch)
+        {           
+            switch (ch)
+            {
+                case 'A':
+                case 'I':
+                case 'J':
+                case 'Q':
+                case 'Y':
+                    return 1;
+                case 'B':
+                case 'K':
+                case 'R':
+                    return 2;                
+                case 'C':
+                case 'G':
+                case 'L':
+                case 'S':
+                    return 3;
+                case 'D':
+                case 'M':
+                case 'T':
+                    return 4;
+                case 'E':
+                case 'H':
+                case 'N':
+                case 'X':
+                    return 5;
+                case 'U':
+                case 'V':
+                case 'W':
+                    return 6;
+                case 'O':
+                case 'Z':                
+                    return 7;
+                case 'F':
+                case 'P':
+                    return 8;
+                default:
+                    return 0;
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBox_Name_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(this, new EventArgs());
+            }
+        }
+    }
+}
